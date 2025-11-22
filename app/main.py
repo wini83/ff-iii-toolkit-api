78e817ee3ca8aa949a78ffafcd7cc34a2fac26cd
wini83 <mariusz.wincior@gmail.com>
2025-11-22 21:12:38 +0100

    fix(api): version form pyproject.toml

--- app/main.py
+++ app/main.py
@@ -3,15 +3,25 @@ from fastapi import FastAPI
 from app.api.file import router as file_router
 from app.api.upload import router as upload_router
 from app.utils.logger import setup_logging
+from fastapi.staticfiles import StaticFiles
+import tomllib
 
 setup_logging()
 
-app = FastAPI(title="Firefly Transaction Tool API")
+def get_version() -> str:
+    with open("pyproject.toml", "rb") as f:
+        data = tomllib.load(f)
+    return data["project"]["version"]
+
+APP_VERSION = get_version()
+
+app = FastAPI(title="Firefly III Alior BLIK Tool", version=APP_VERSION)
 
 app.include_router(upload_router)
 app.include_router(file_router)
 
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
-@app.get("/")
-def root():
-    return {"status": "OK", "message": "API działa 🚀"}
+app.get("/health")
+async def health_check():
+    return {"status": "ok"}
