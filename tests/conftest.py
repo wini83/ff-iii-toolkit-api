bccdc746d3f1082bcd3218c0f3bcc41a905b2aab
wini83 <mariusz.wincior@gmail.com>
2026-01-17 08:14:20 +0100

    test(db): add initial tests for user repository

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,26 @@
 import pytest
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 from main import app
+from services.db.models import Base
 
 
 @pytest.fixture()
 def client() -> TestClient:
     return TestClient(app)
+
+
+@pytest.fixture()
+def db_session():
+    engine = create_engine("sqlite:///:memory:")
+    TestingSessionLocal = sessionmaker(bind=engine)
+
+    Base.metadata.create_all(bind=engine)
+
+    session = TestingSessionLocal()
+    try:
+        yield session
+    finally:
+        session.close()
