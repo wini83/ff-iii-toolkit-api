5201b4be7ab91da4f603fd5dece29022f52d8ce0
wini83 <mariusz.wincior@gmail.com>
2026-01-19 19:42:05 +0100

    test(settings): fix conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,6 @@
+import os
+
+os.environ.setdefault("SECRET_KEY", "test-secret")
 import pytest
 from fastapi import Request
 from fastapi.testclient import TestClient
@@ -8,6 +11,8 @@ from sqlalchemy.pool import StaticPool
 from api.deps_db import get_db
 from main import create_app  # 👈 TO JEST KLUCZ
 from services.db.models import Base
+
+os.environ.setdefault("SECRET_KEY", "test-secret")
 
 
 @pytest.fixture(scope="function")
